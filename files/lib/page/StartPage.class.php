5551c6230f7f1e8c3176814c14aae9880ba00e84
huner32 <pb_9572@gmx.de>
2013-07-21 21:58:27 +0200

    Update

---
+++ files/lib/page/StartPage.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace wcf\page;
+use wcf\system\WCF;
+
+/**
+ * @author	Philipp Bornemann
+ * @copyright	2013 Philipp Bornemann
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class StartPage extends AbstractPage {
+	/**
+	 * @see	wcf\page\AbstractPage::$activeMenuItem
+	 */
+	public $activeMenuItem = 'wcf.header.menu.startpage';
+}
+?>
