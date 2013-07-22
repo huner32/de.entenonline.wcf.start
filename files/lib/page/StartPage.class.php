7bb7458c6a6bc3ef650df2ffc6f106d5fb92aa83
huner32 <pb_9572@gmx.de>
2013-07-22 13:13:34 +0200

    Fix index breadcrumb

--- files/lib/page/StartPage.class.php
+++ files/lib/page/StartPage.class.php
@@ -11,5 +11,15 @@ class StartPage extends AbstractPage {
 	 * @see	wcf\page\AbstractPage::$activeMenuItem
 	 */
 	public $activeMenuItem = 'wcf.header.menu.startpage';
+	
+	/**
+	 * @see \wcf\page\IPage::show()
+	 */
+	public function show() {
+		// remove index breadcrumb
+		WCF::getBreadcrumbs()->remove(0);
+
+		parent::show();
+	}
 }
 ?>
